4f2fd3776708d883c8b12f914222d1a4ec01454b
TruffleBug <lisa.ho1022@gmail.com>
2024-11-22 15:58:24 -0700

    Project complete

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # webpack-template
+https://www.theodinproject.com/lessons/node-path-javascript-weather-app
--- src/index.js
+++ src/index.js
@@ -1,12 +1,74 @@
-import './styles.css'
-import { greeting } from './greeting.js'
+import './styles.css';
 
-console.log(greeting)
+const toggleTempDisplayButton = document.querySelector(
+    '.toggleTempDisplayButton'
+);
+const locationText = document.querySelector('.location');
+const conditionText = document.querySelector('.condition');
+const tempText = document.querySelector('.temp');
+const descriptionText = document.querySelector('.description');
+const form = document.querySelector('form');
+const gif = document.querySelector('img');
 
-//to add a local image to js file
-import odinImage from './odin.png'
+let tempDisplay = 'F';
+let tempInCDisplay;
+let tempInFDisplay;
 
-const image = document.createElement('img')
-image.src = odinImage
+toggleTempDisplayButton.addEventListener('click', (event) => {
+    if (tempDisplay === 'F') {
+        toggleTempDisplayButton.textContent = 'Display Fahrenheit';
+        tempDisplay = 'C';
+        tempText.textContent = `${tempInCDisplay}\u00B0C`;
+    } else {
+        toggleTempDisplayButton.textContent = 'Display Celsius';
+        tempDisplay = 'F';
+        tempText.textContent = `${tempInFDisplay}\u00B0F`;
+    }
+});
 
-document.body.appendChild(image)
+async function getWeather(inputCity) {
+    try {
+        const weatherResult = await fetch(
+            `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${inputCity}?key=4BUGEMNKKM546V67VVUDNNCEQ`
+        );
+        const weatherData = await weatherResult.json();
+        const location = weatherData.resolvedAddress;
+        const condition = weatherData.currentConditions.conditions;
+        const tempInF = weatherData.currentConditions.temp;
+        const description = weatherData.description;
+        const tempInC = (((tempInF - 32) * 5) / 9).toFixed(1);
+
+        onGetWeatherSuccess(location, condition, tempInF, description, tempInC);
+
+        const gifResult = await fetch(
+            `https://api.giphy.com/v1/gifs/translate?api_key=JTHIb1Wj4xHUZ0VdtQjs005ilAnCBwg6&s=${condition}`
+        );
+        const gifData = await gifResult.json();
+
+        gif.src = gifData.data.images.original.url;
+    } catch (error) {
+        alert('Invalid location');
+    }
+}
+
+function onGetWeatherSuccess(
+    location,
+    condition,
+    tempInF,
+    description,
+    tempInC
+) {
+    locationText.textContent = location;
+    conditionText.textContent = condition;
+    tempText.textContent = `${tempInF}\u00B0F`;
+    descriptionText.textContent = description;
+    tempInFDisplay = tempInF;
+    tempInCDisplay = tempInC;
+}
+
+getWeather('denver');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    getWeather(document.getElementById('locationInput').value);
+});
